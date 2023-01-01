99961f66cad1e233de8ea511d972683bc3a45c4a
junya.nomura <unijackcofee@gmail.com>
2023-01-01 18:42:26 +0900

    updated

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
+const connectDB = require('./db/connect');
+require('dotenv').config();
+app.use(express.json());
 
 const taskRoutes = require('./routes/tasks');
 const PORT = 5000;
 
 app.use('/api/v1/tasks', taskRoutes);
 
-app.listen(PORT, () => {
-  console.log('server activated');
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(PORT, () => {
+      console.log('server activated');
+    });
+  } catch(err) {
+    console.log(err);
+  };
+};
+
+start();
--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,9 +1,18 @@
+const Task = require('../models/Task');
+
 const getAllTasks = (req, res) => {
   res.send('タスクをすべて取得しました');
 };
 
-const createTask = (req, res) => {
-  res.send('タスクを新規作成しました');
+const createTask = async (req, res) => {
+  try {
+    const createTask = await Task.create(req.body);
+    res.status(200).json(createTask);
+    console.log('success')
+  } catch (err) {
+    console.log('error')
+    res.status(500).json(err);
+  };
 };
 
 const getSingleTask = (req, res) => {
