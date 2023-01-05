429336ad1281776a7f0e91f8455d97a63d279d31
野村淳也 <unijack@unijackMacMini.local>
2023-01-06 00:37:53 +0900

    script modified

--- public/script.js
+++ public/script.js
@@ -1,4 +1,6 @@
 const tasksDOM = document.querySelector('.tasks');
+const formDOM = document.querySelector('.task-form');
+const taskInputDOM = document.querySelector('.task-input');
 
 // /api/v1/tasksからタスクを読み込む
 const showTasks = async () => {
@@ -17,7 +19,7 @@ const showTasks = async () => {
         name,
       } = task;
 
-      return `<div class="single-task">
+      return `<div class="single-task aaaaaa">
       <h5>
         <span>
           <i class="far fa-check-circle"></i>
@@ -30,12 +32,12 @@ const showTasks = async () => {
           <i class="fas fa-edit"></i>
         </a>
         <!-- ゴミ箱リンク -->
-        <button type="button" class="delete-btn">
+        <button type="button" class="delete-btn" data-id="${_id}">
           <i class="fas fa-trash"></i>
         </button>
       </div>
     </div>`;
-    })  
+    });  
     tasksDOM.innerHTML = allTasks.join('');
     console.log(allTasks);
   } catch (err) {
@@ -44,3 +46,34 @@ const showTasks = async () => {
 };
 
 showTasks();
+
+//タスクを新規作成する
+formDOM.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  const name = taskInputDOM.value;
+  try {
+    await axios.post('/api/v1/tasks', {
+      name: name
+    });
+    showTasks();
+    taskInputDOM.value = '';
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//タスクを削除する
+tasksDOM.addEventListener('click', async (event) => {
+  event.preventDefault();
+  const element = event.target;
+  // console.log(element.parentElement.classList);
+  if (element.parentElement.classList.contains('delete-btn')) {
+    const id = element.parentElement.dataset.id;
+    try {
+      await axios.delete(`/api/v1/tasks/${id}`);
+      showTasks();
+    } catch (err) {
+      console.log(err)
+    }
+  }
+})
